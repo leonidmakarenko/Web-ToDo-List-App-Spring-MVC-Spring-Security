5cba1269d55e12999a4ba9fdd60bccfb23b0df9f
Leonid Makarenko <l.makarenko@zohomail.com>
2022-12-04 17:02:04 +0200

    initial

---
+++ src/main/java/leo/springwebapp/spring_web_todolist/WebToDoListApplication.java
@@ -0,0 +1,13 @@
+package leo.springwebapp.spring_web_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebToDoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebToDoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/leo/springwebapp/spring_web_todolist/WebToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package leo.springwebapp.spring_web_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
